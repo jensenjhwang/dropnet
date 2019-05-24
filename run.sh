1974c835b8285a0da2643957e3d6436bd6b9bf52
Wang Jinhui <wangjh97@stanford.edu>
2019-05-23 17:37:20 -0700

    test

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ OUTPUT_PATH=$PWD"/test_output/*"
 NOW=$(date '+%d_%m_%Y_%H_%M_%S')
 
 # python command
-python3 train.py \
+python3 -m trainer.train \
     --job-dir="output_${NOW}" \
-    --train_steps=1000 \
+    --train_steps=7500 \
     --batch_size=16 \
     --hparams="hidden_units=[100, 100, 100],
-      dropouts=[0.5, 0.5, 0.5],
-      samples=100"
+      dropouts=[0.2, 0.2, 0.2],
+      samples=200,
+      drop_type=INVERTED,
+      activation=tanh"
