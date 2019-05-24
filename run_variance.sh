6be2525c3b75d09ee16bf80844407712e29e99d5
philhchen <philchen2000@gmail.com>
2019-05-23 19:12:32 -0700

    Modified .sh

--- run_variance.sh
+++ run_variance.sh
@@ -5,7 +5,7 @@ NOW=$(date '+%d_%m_%Y_%H_%M_%S')
 
 # python command
 python3 -m variance.variance \
-    --job-dir="output_${NOW}" \
+    --job-dir="${OUTPUT_PATH}/output_${NOW}" \
     --train_steps=5 \
     --batch_size=16 \
     --hparams="hidden_units=[100, 100, 100],
